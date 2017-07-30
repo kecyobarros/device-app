a1ec7b63a6823ff1997865292182d10c4f0f1960
Kecyo Barros <kecyobarros@gmail.com>
2017-07-29 21:58:28 -0300

    Adicionando o profile de teste

--- src/main/java/br/com/kecyo/deviceapp/gateways/repository/mongo/DeviceRepository.java
+++ src/main/java/br/com/kecyo/deviceapp/gateways/repository/mongo/DeviceRepository.java
@@ -1,8 +1,10 @@
 package br.com.kecyo.deviceapp.gateways.repository.mongo;
 
 import br.com.kecyo.deviceapp.entities.Device;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Profile("!test")
 public interface DeviceRepository extends MongoRepository<Device, String> {}
